3fc0b93d816b29f9a4c75e1236e8ec4be747f5a5
mk <munekawa0807@outlook.jp>
2020-07-04 17:24:11 +0900

    Initial Commit

---
+++ taskApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  taskApp
+//
+//  Created by Keita Munekawa on 2020/07/04.
+//  Copyright © 2020 KM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
