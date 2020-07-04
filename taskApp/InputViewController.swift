4cc46edad37e962e1b0c13016ac8bcd20c3a4042
mk <munekawa0807@outlook.jp>
2020-07-04 18:26:17 +0900

    pod installとgitignoreの作成まで実施

---
+++ taskApp/InputViewController.swift
@@ -0,0 +1,27 @@
+//
+//  InputViewController.swift
+//  taskApp
+//
+//  Created by Keita Munekawa on 2020/07/04.
+//  Copyright © 2020 KM. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+    @IBOutlet var titleTextField: UITextField!
+    
+    @IBOutlet var contentsTextView: UITextView!
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
