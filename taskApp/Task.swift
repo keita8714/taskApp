e89de89dbacb0921683a4a0580edbd33ea9618d2
mk <munekawa0807@outlook.jp>
2020-07-18 14:49:40 +0900

    変更

---
+++ taskApp/Task.swift
@@ -0,0 +1,31 @@
+//
+//  Task.swift
+//  taskApp
+//
+//  Created by Keita Munekawa on 2020/07/04.
+//  Copyright © 2020 KM. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    
+    var category = ["study","work","music","news"]
+    
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
